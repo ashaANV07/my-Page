3630be704c6ed90d271079c05f76bcaa5a1f4123
ashaANV07 <asha.c@anvtech.co>
2024-01-19 11:59:06 +0545

    Give Grid to all Components

--- src/App.js
+++ src/App.js
@@ -14,45 +14,30 @@ import {
 function App() {
   return (
     <Grid container spacing={2}>
-      {/* <Grid container> */}
       <Grid item xs={12} md={12}>
-        {/* <Box
-          sx={{
-            width: 680,
-            height: 100,
-            borderRadius: 1,
-            bgcolor: "#c8e6c9",
-          }}
-        > */}
         <Typography
           variant="subtitle1"
           gutterBottom
           sx={{ borderRadius: 1, bgcolor: "#c8e6c9" }}
         >
           Note:
-        </Typography>
-        <Typography
-          variant="subtitle1"
-          gutterBottom
-          sx={{ borderRadius: 1, bgcolor: "#c8e6c9" }}
-        >
+          <br />
           IPO window will remain open from 10:00 AM till 5:00 PM on trading
           days. you can accept the UPI mandate request till 5:00 PM on the day
           off IPO close date. If you don't recieve the UPI request till the end
           of the day due to delays from the bank, kindlly delete and apply again
         </Typography>
-        {/* </Box> */}
         <br />
       </Grid>
 
       <Grid container spacing={5} alignItems="center" justifyContent="center">
         <Grid item xs={12} sm={6}>
           <Page3
-            sx={{
-              height: 140,
-              width: 100,
-              border: "2px solid black",
-            }}
+            // sx={{
+            //   height: 140,
+            //   width: 100,
+            //   border: "2px solid black",
+            // }}
           />
         </Grid>
         <Grid item xs={12} sm={6}>
--- src/Components/Page4.js
+++ src/Components/Page4.js
@@ -1,102 +1,93 @@
 import React from "react";
-import { TextField, Typography, Container, Box } from "@mui/material";
+import { TextField, Typography, Container, Box, Grid } from "@mui/material";
 
 export const Page4 = () => {
   return (
-    <Box sx={{ border: 1, width: 300, height: 550 }} justifyContent="center">
-      <Container maxWidth="sm" justifyContent="center" alignItems="center">
-        <div>
-          <Typography variant="h5" gutterBottom>
-            Personal Details
-          </Typography>
-          <Typography variant="subtitle1" gutterBottom>
-            CDSL number
-          </Typography>
-          <TextField
-            id="outlined-basic"
-            label="Enter CDSL number"
-            variant="outlined"
-            size="small"
-            sx={{
-              width: 250,
-              height: 30,
-            }}
-          />
-          <br /> <br />
-          <Typography variant="subtitle1" gutterBottom>
-            Pan
-          </Typography>
-          <TextField
-            id="outlined-basic"
-            label="Enter Pan number"
-            variant="outlined"
-            size="small"
-            sx={{
-              width: 250,
-              height: 30,
-            }}
-          />
-          <br /> <br />
-          <Typography variant="subtitle1" gutterBottom>
-            Name
-          </Typography>
-          <TextField
-            id="outlined-basic"
-            label="Enter name"
-            variant="outlined"
-            size="small"
-            sx={{
-              width: 250,
-              height: 30,
-            }}
-          />
-          <br /> <br />
-          <Typography variant="subtitle1" gutterBottom>
-            Mobile Number
-          </Typography>
-          <TextField
-            type="number"
-            id="outlined-basic"
-            label="Enter Mobile number"
-            variant="outlined"
-            size="small"
-            sx={{
-              width: 250,
-              height: 30,
-            }}
-          />
-          <br /> <br />
-          <Typography variant="subtitle1" gutterBottom>
-            Email
-          </Typography>
-          <TextField
-            type="email"
-            id="outlined-basic"
-            label="Enter your email"
-            variant="outlined"
-            size="small"
-            sx={{
-              width: 250,
-              height: 30,
-            }}
-          />
-          <br /> <br />
-          <Typography variant="subtitle1" gutterBottom>
-            Bank UPI
-          </Typography>
-          <TextField
-            type="email"
-            id="outlined-basic"
-            label="Enter Bank UPI"
-            variant="outlined"
-            size="small"
-            sx={{
-              width: 250,
-              height: 30,
-            }}
-          />
-        </div>
-      </Container>
-    </Box>
+    <Container component="main" maxWidth="lg">
+      <Box sx={{ border: 1, height: 600, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <Typography variant="h5" gutterBottom>
+              Personal Details
+            </Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <Typography variant="subtitle1" gutterBottom>
+              CDSL number
+            </Typography>
+            <TextField
+              id="outlined-basic"
+              label="Enter CDSL number"
+              variant="outlined"
+              size="small"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Typography variant="subtitle1" gutterBottom>
+              Pan
+            </Typography>
+            <TextField
+              id="outlined-basic"
+              label="Enter Pan number"
+              variant="outlined"
+              size="small"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Typography variant="subtitle1" gutterBottom>
+              Name
+            </Typography>
+            <TextField
+              id="outlined-basic"
+              label="Enter name"
+              variant="outlined"
+              size="small"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Typography variant="subtitle1" gutterBottom>
+              Mobile Number
+            </Typography>
+            <TextField
+              type="number"
+              id="outlined-basic"
+              label="Enter Mobile number"
+              variant="outlined"
+              size="small"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Typography variant="subtitle1" gutterBottom>
+              Email
+            </Typography>
+            <TextField
+              type="email"
+              id="outlined-basic"
+              label="Enter your email"
+              variant="outlined"
+              size="small"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Typography variant="subtitle1" gutterBottom>
+              Bank UPI
+            </Typography>
+            <TextField
+              type="email"
+              id="outlined-basic"
+              label="Enter Bank UPI"
+              variant="outlined"
+              size="small"
+              fullWidth
+            />
+          </Grid>
+        </Grid>
+      </Box>
+    </Container>
   );
 };
